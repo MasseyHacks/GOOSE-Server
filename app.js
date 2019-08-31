0bf33ef308605ab0c8ab2d334c83724ef6ea96aa
James Xu <xujames007@gmail.com>
2019-08-31 14:23:04 -0400

    replaced mongo logs

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoose        = require('mongoose');
 const port            = process.env.PORT || 3005;
 const database        = process.env.DATABASE || 'mongodb://localhost:27017/goose';
 const logger          = require('./app/services/logger');
+const lw              = require('@google-cloud/logging-winston');
+const winston         = require('winston');
 
 // Start configuration
 const organizers      = require('./config/organizers');
@@ -35,7 +37,28 @@ mongoose.connect(database,
 
 stats.startService();
 
+// const requestLogger = winston.createLogger({
+//     format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+//     transports: [
+//         new lw.LoggingWinston()
+//     ]
+// })
+//
+// function logRequests(req, res, next) {
+//     requestLogger.debug(`${req.url} endpoint hit`, {
+//         httpRequest: {
+//             status: res.statusCode,
+//             requestUrl: req.url,
+//             requestMethod: req.method,
+//             remoteIp: req.connection.remoteAddress,
+//             // etc.
+//         }
+//     });
+//     next()
+// }
+
 app.use(cors());
+// app.use(logRequests);
 
 let githubRouter = express.Router();
 require('./app/routes/github')(githubRouter);
--- app/services/logger.js
+++ app/services/logger.js
@@ -2,6 +2,7 @@ const LogEvent        = require('../models/LogEvent');
 const axios           = require('axios');
 const winston         = require('winston');
 const {LoggingWinston}= require('@google-cloud/logging-winston');
+const User            = require('../models/User')
 
 //const Raven           = require('raven');
 function createWinstonLogger() {
@@ -23,14 +24,14 @@ function createWinstonLogger() {
             // - Write to all logs with level `info` and below to `combined.log`
             // - Write all logs error (and below) to `error.log`.
             //
-            new winston.transports.File({ filename: 'error.log' , level:'error'}),
+            new winston.transports.File({ filename: 'error.log' , level:'error', handleExceptions: true}),
             new winston.transports.File({ filename: 'combined.log' }),
             loggingWinston
-        ],
-        exceptionHandlers: [
-            new winston.transports.File({ filename: 'exceptions.log' }),
-            loggingWinston
-        ]
+        ]//,
+        // exceptionHandlers: [
+        //     new winston.transports.File({ filename: 'exceptions.log' }),
+        //     loggingWinston
+        // ]
     });
     winston.loggers.add('default', logger);
     //
@@ -39,10 +40,28 @@ function createWinstonLogger() {
     //
     if (process.env.NODE_ENV !== 'production') {
         logger.add(new winston.transports.Console({
-            format: winston.format.simple()
+            format: winston.format.simple(),
+            handleExceptions: true
         }));
     }
     return logger;
+}
+
+async function getLogActor(id) {
+    try {
+        const doc = await User.findById(id);
+        return {
+            _id: id.toString(),
+            email: doc.email,
+            name: doc.fullName
+        }
+    } catch (e) {
+        return {
+            _id: -1,
+            email: 'internal@masseyhacks.ca',
+            name: 'MasseyHacks Internal Authority'
+        }
+    }
 }
 
 module.exports = {
@@ -55,7 +74,7 @@ module.exports = {
                 // Keep everyone happy
                 if (process.env.NODE_ENV === 'production'){
 
-                    var data =  'Request: ' + req.method + ' ' + req.url + '\n' +
+                    let data =  'Request: ' + req.method + ' ' + req.url + '\n' +
                                 'User: ' + req.userExecute['fullName'] + ' ' + req.userExecute['ip'] + '\n' +
                                 '\n -------------------------- \n' +
                                 'Body: \n ' +
@@ -64,7 +83,7 @@ module.exports = {
                                 '\nError:\n' +
                                 JSON.stringify(err, null, 2) +
                                 '``` \n';
-                    this.defaultLogger.alert(data.replace('\n', ' '));
+                    this.defaultLogger.error(data.replace('\n', ' '));
                     /*if (process.env.SERVER_RAVEN_KEY && (!err.code || err.code >= 500)) {
                         Raven.captureMessage(data, {
                             level: 'error'
@@ -102,69 +121,90 @@ module.exports = {
             }
         };
     },
-    logAction : function (actionFrom, actionTo, message, detailedMessage, cb) {
+    logAction : async function (actionFrom, actionTo, message, detailedMessage, cb) {
         // Start bash
-        this.defaultLogger.info(`timestamp: ${Date.now()}, idTo: ${actionTo}, idFrom: ${actionFrom}, message: ${message}, detailedMessage: ${detailedMessage}`);
-        LogEvent
-            .create({
-                'to.ID': actionTo,
-                'from.ID': actionFrom,
-                'message': message,
-                'detailedMessage': detailedMessage,
-                'timestamp': Date.now()
-            }, function (err, e) {
+        // this.defaultLogger.info(`timestamp: ${Date.now()}, idTo: ${actionTo}, idFrom: ${actionFrom}, message: ${message}, detailedMessage: ${detailedMessage}`);
+        let fromData = getLogActor(actionFrom);
+        let toData = getLogActor(actionTo);
+        fromData = await fromData;
+        toData = await toData;
+        console.log("fromData: " + JSON.stringify(fromData));
+        console.log("toData: " + JSON.stringify(toData));
 
-                LogEvent
-                    .findOne({_id: e._id})
-                    .populate(actionFrom === -1 ? '' : 'fromUser') // Only populate if user exists
-                    .populate(actionTo === -1 ? '' : 'toUser')
-                    .exec(function (err, event) {
+        this.defaultLogger.info(`From: ${fromData['email']}, To: ${toData['email']}, message: ${message}`, {
+            details: {
+                from: fromData,
+                to: toData,
+                message: message,
+                detailedMessage: detailedMessage,
+                time: Date.now()
+            }
+        });
 
-                        console.log(event);
+        if (cb) {
+            cb()
+        }
 
-                        if (event) {
-                            LogEvent.findOneAndUpdate({
-                                _id: event._id
-                            }, {
-                                'from.name': actionFrom === -1 ? 'MasseyHacks Internal Authority' : event.fromUser !== null ? event.fromUser.fullName : 'Unable to get name',
-                                'from.email': actionFrom === -1 ? 'internal@masseyhacks.ca' : event.fromUser !== null ? event.fromUser.email : 'Unable to get name',
-                                'to.name': actionTo === -1 ? 'MasseyHacks Internal Authority' : event.toUser !== null ? event.toUser.fullName : 'Unable to get name',
-                                'to.email': actionTo === -1 ? 'internal@masseyhacks.ca' : event.toUser !== null ? event.toUser.email : 'Unable to get email'
-                            }, {
-                                new: true
-                            }, function (err, newEvent) {
-
-                                console.log(newEvent);
-
-                                if (process.env.NODE_ENV === 'production' && process.env.AUDIT_SLACK_HOOK) {
-                                    console.log('Sending audit log...');
-
-                                    axios.post(process.env.AUDIT_SLACK_HOOK,
-                                        {
-                                            form: {
-                                                payload: JSON.stringify({
-                                                    'icon_emoji': ':pcedoris:',
-                                                    'username': 'AuditBot',
-                                                    'text': '```' + newEvent + '```'
-                                                })
-                                            }
-                                        },
-                                        function (error, response, body) {
-
-                                        }
-                                    ).then(() => console.log('Message sent to slack'));
-
-                                }
-
-                                if (cb) {
-                                    cb();
-                                }
-                            })
-                        } else {
-                            console.log('Logging fail.')
-                        }
-                    });
-
-            })
+        // LogEvent
+        //     .create({
+        //         'to.ID': actionTo,
+        //         'from.ID': actionFrom,
+        //         'message': message,
+        //         'detailedMessage': detailedMessage,
+        //         'timestamp': Date.now()
+        //     }, function (err, e) {
+        //
+        //         LogEvent
+        //             .findOne({_id: e._id})
+        //             .populate(actionFrom === -1 ? '' : 'fromUser') // Only populate if user exists
+        //             .populate(actionTo === -1 ? '' : 'toUser')
+        //             .exec(function (err, event) {
+        //
+        //                 console.log(event);
+        //
+        //                 if (event) {
+        //                     LogEvent.findOneAndUpdate({
+        //                         _id: event._id
+        //                     }, {
+        //                         'from.name': actionFrom === -1 ? 'MasseyHacks Internal Authority' : event.fromUser !== null ? event.fromUser.fullName : 'Unable to get name',
+        //                         'from.email': actionFrom === -1 ? 'internal@masseyhacks.ca' : event.fromUser !== null ? event.fromUser.email : 'Unable to get name',
+        //                         'to.name': actionTo === -1 ? 'MasseyHacks Internal Authority' : event.toUser !== null ? event.toUser.fullName : 'Unable to get name',
+        //                         'to.email': actionTo === -1 ? 'internal@masseyhacks.ca' : event.toUser !== null ? event.toUser.email : 'Unable to get email'
+        //                     }, {
+        //                         new: true
+        //                     }, function (err, newEvent) {
+        //
+        //                         console.log(newEvent);
+        //
+        //                         if (process.env.NODE_ENV === 'production' && process.env.AUDIT_SLACK_HOOK) {
+        //                             console.log('Sending audit log...');
+        //
+        //                             axios.post(process.env.AUDIT_SLACK_HOOK,
+        //                                 {
+        //                                     form: {
+        //                                         payload: JSON.stringify({
+        //                                             'icon_emoji': ':pcedoris:',
+        //                                             'username': 'AuditBot',
+        //                                             'text': '```' + newEvent + '```'
+        //                                         })
+        //                                     }
+        //                                 },
+        //                                 function (error, response, body) {
+        //
+        //                                 }
+        //                             ).then(() => console.log('Message sent to slack'));
+        //
+        //                         }
+        //
+        //                         if (cb) {
+        //                             cb();
+        //                         }
+        //                     })
+        //                 } else {
+        //                     console.log('Logging fail.')
+        //                 }
+        //             });
+        //
+        //     })
     }
 };
