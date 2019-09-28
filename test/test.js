16a642c79545528b5a7c10e54bd51c726fe69698
James Xu <36768789+jamesxu123@users.noreply.github.com>
2019-09-28 15:58:53 -0400

    Remove useless "unit tests"

--- test/test.js
+++ test/test.js
@@ -6,58 +6,58 @@ const User           = require('../app/models/User');
 const logger         = require('../app/services/logger');
 require('dotenv').config();
 
-describe('Database and Mongoose Tests', function () {
-    describe("Initial Connection", function () {
-        it('should connect successfully', function (done) {
-            mongoose.connect('mongodb://localhost:27017/gooseTest', {
-                auto_reconnect: true,
-                useNewUrlParser: true,
-                useFindAndModify: false,
-                useCreateIndex: true
-            }).then(() => {
-                // console.log("DONNEEEEE");
-                done()
-            }).catch(error => {
-                // assert.strictEqual(error, null);
-                done(error)
-            })
-        })
-    });
+// describe('Database and Mongoose Tests', function () {
+//     describe("Initial Connection", function () {
+//         it('should connect successfully', function (done) {
+//             mongoose.connect('mongodb://localhost:27017/gooseTest', {
+//                 auto_reconnect: true,
+//                 useNewUrlParser: true,
+//                 useFindAndModify: false,
+//                 useCreateIndex: true
+//             }).then(() => {
+//                 // console.log("DONNEEEEE");
+//                 done()
+//             }).catch(error => {
+//                 // assert.strictEqual(error, null);
+//                 done(error)
+//             })
+//         })
+//     });
 
-    describe('Initialize settings', function () {
-        it('should initialize organizers', function () {
-            const organizers      = require('../config/organizers');
-        });
-        it('should initialize settings', function () {
-            const settings        = require('../config/settings');
-        })
-    });
+//     describe('Initialize settings', function () {
+//         it('should initialize organizers', function () {
+//             const organizers      = require('../config/organizers');
+//         });
+//         it('should initialize settings', function () {
+//             const settings        = require('../config/settings');
+//         })
+//     });
 
-    describe('Users', function () {
-        describe('Creating a user', function () {
-            it('should create a user successfully', function (done) {
-                UserController.createUser('tester@masseyhacks.ca', 'Test', 'User', 'test123', function (error) {
-                    done(error);
-                })
-            });
-        });
-        describe('Remove user', function () {
-            it('should delete the created user using its email', function (done) {
-                User.getByEmail('tester@masseyhacks.ca', function (error, user) {
-                    if (!error) {
-                        user.remove();
-                        done();
-                    } else {
-                        done(error);
-                    }
-                })
-            })
-        })
-    });
-    describe('Hardware system', function () {
+//     describe('Users', function () {
+//         describe('Creating a user', function () {
+//             it('should create a user successfully', function (done) {
+//                 UserController.createUser('tester@masseyhacks.ca', 'Test', 'User', 'test123', function (error) {
+//                     done(error);
+//                 })
+//             });
+//         });
+//         describe('Remove user', function () {
+//             it('should delete the created user using its email', function (done) {
+//                 User.getByEmail('tester@masseyhacks.ca', function (error, user) {
+//                     if (!error) {
+//                         user.remove();
+//                         done();
+//                     } else {
+//                         done(error);
+//                     }
+//                 })
+//             })
+//         })
+//     });
+//     describe('Hardware system', function () {
 
-    });
-});
+//     });
+// });
 
 describe('Logging system test', function () {
     it('Winston logger should exist', function (done) {
